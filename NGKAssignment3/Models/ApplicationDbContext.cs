35b4b526aa7ee58f183d456f05a4bddea1f773a8
thomasgl710 <thomasgl10@gmail.com>
2020-11-19 10:35:59 +0100

    Nice

---
+++ NGKAssignment3/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace NGKAssignment3.Models
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<NGKAssignment3.Models.WeatherStation> WeatherStation { get; set; }
+    }
+}
---
+++ NGKAssignment3/Models/WeatherStation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NGKAssignment3.Models
+{
+    public class WeatherStation
+    {
+        public WeatherStation(string name, int lat, int lon)
+        {
+            Name = name;
+            Lat = lat;
+            Lon = lon;
+        }
+        public string Name{ get; set;}
+        public int Lat { get; set; }
+        public int Lon { get; set; }
+        public DateTime Time { get; set; }
+        public float Temperatur { get; set; }
+        public int Humidity { get; set; }
+        public float AirPressure { get; set; }
+
+    }
+}
