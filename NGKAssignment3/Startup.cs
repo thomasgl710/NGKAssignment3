881cb2ef321f1d9b315718de7b7bdae1050e4749
thomasgl710 <thomasgl10@gmail.com>
2020-11-19 10:48:56 +0100

    AppDbContext

--- NGKAssignment3/Startup.cs
+++ NGKAssignment3/Startup.cs
@@ -24,6 +24,7 @@ namespace NGKAssignment3
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
